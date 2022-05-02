377e74be5c7f6b6961836d110a7f7889d6294bfe
Alex Paredes <alexparedesr95@gmail.com>
2022-05-01 22:55:10 -0500

    REFACTOR: Removed unused dependencies.

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Navigate, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
