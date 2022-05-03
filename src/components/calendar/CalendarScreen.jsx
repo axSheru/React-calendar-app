57645c2af74e6b1c45093072ffe9022d75fb31d7
Alex Paredes <alexparedesr95@gmail.com>
2022-05-03 12:42:49 -0500

    FEAT: Applied personalized styles to calendar events and applied language translation.

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import 'moment/locale/es-mx';
 
 import { Navbar } from '../ui/Navbar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
 
+moment.locale( 'es-mx' );
 
 const localizer = momentLocalizer( moment );
 
@@ -12,10 +15,26 @@ const events = [{
     title: 'Cumpleaños del jefe',
     start: moment().toDate(),
     end: moment().add( 2, 'hours' ).toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel'
 }];
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        
+        const style = {
+            backgroundColor: '#BF33FF',
+            borderRadius: '10px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        };
+
+        return {style};
+
+    };
+
     return (
         <div className='calendar-screen'>
             <Navbar />
@@ -24,6 +43,8 @@ export const CalendarScreen = () => {
                 events={ events }
                 startAccessor='start'
                 endAccessor='end'
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
             />
         </div>
     );
