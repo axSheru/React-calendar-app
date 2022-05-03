40f273550a793a70a9fcf2793a4cf954e0db0d4a
Alex Paredes <alexparedesr95@gmail.com>
2022-05-02 20:25:01 -0500

    FEAT: Imported general styles file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRouter } from './routers/AppRouter';
+import './styles/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
