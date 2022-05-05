9c6109409fcc09faf4ab21fa869f94615802da47
Alex Paredes <alexparedesr95@gmail.com>
2022-05-04 23:06:50 -0500

    FEAT: Created calendar events skeleton and used localStorage to save last view.

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/es-mx';
@@ -6,6 +6,7 @@ import 'moment/locale/es-mx';
 import { Navbar } from '../ui/Navbar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
 
 moment.locale( 'es-mx' );
 
@@ -16,10 +17,29 @@ const events = [{
     start: moment().toDate(),
     end: moment().add( 2, 'hours' ).toDate(),
     bgcolor: '#fafafa',
-    notes: 'Comprar el pastel'
+    notes: 'Comprar el pastel',
+    user: {
+        _id: '123',
+        name: 'Alex'
+    }
 }];
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState( localStorage.getItem( 'lastView' ) || 'month' );
+
+    const onDoubleClick = ( e ) => {
+        console.log(e);
+    };
+
+    const onSelectEvent = ( e ) => {
+        console.log(e);
+    };
+
+    const onViewChange = ( e ) => {
+        setLastView( e );
+        localStorage.setItem( 'lastView', e );
+    };
 
     const eventStyleGetter = ( event, start, end, isSelected ) => {
         
@@ -45,6 +65,13 @@ export const CalendarScreen = () => {
                 endAccessor='end'
                 messages={ messages }
                 eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
             />
         </div>
     );
