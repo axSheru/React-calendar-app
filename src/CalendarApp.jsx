ac76c4e7080ec5fdd6c2a9e07ab6b644aa6ef044
Alex Paredes <alexparedesr95@gmail.com>
2022-05-10 16:14:53 -0500

    FEAT: Imported Provider with store and AppRouter into main component.

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
+import { AppRouter } from './routers/AppRouter';
 
 export const CalendarApp = () => {
     return (
-        <h1>CalendarApp</h1>
+        <Provider store={ store }>
+            <AppRouter />
+        </Provider>
     );
 };
