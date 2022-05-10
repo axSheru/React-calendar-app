9319e3b4c5d5430b6b09a699aa0f56eb4b0a2ff6
Alex Paredes <alexparedesr95@gmail.com>
2022-05-10 16:14:29 -0500

    REFACTOR: Imported only CalendarApp component in index.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppRouter } from './routers/AppRouter';
+import { CalendarApp } from './CalendarApp';
 import './styles/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <AppRouter />
-  </div>
+  <CalendarApp />
 );
