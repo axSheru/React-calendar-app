fec7ec6e495177066d5333b88dd8e101aa81c511
Alex Paredes <alexparedesr95@gmail.com>
2022-05-13 13:38:01 -0500

    FEAT: Created case for event update con eventReducer. Added id to inital event state.

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -4,6 +4,7 @@ import { types } from '../types/types';
 const initialState = {
     events: [
         {
+            id: new Date().getTime(),
             title: 'Cumpleaños del jefe',
             start: moment().toDate(),
             end: moment().add( 2, 'hours' ).toDate(),
@@ -41,6 +42,14 @@ export const eventReducer = ( state = initialState, action ) => {
                     ...state.events,
                     action.payload
                 ]
+            };
+
+        case types.eventUpdate:
+            return {
+                ...state,
+                events: state.events.map(
+                    e => ( e.id === action.payload.id ) ? action.payload : e
+                )
             };
     
         default:
