3eb6eb167199c2febea1b2e740a54538cf4d666d
Alex Paredes <alexparedesr95@gmail.com>
2022-05-30 22:28:33 -0500

    FEAT: Created authReducer.

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+const initalState = {
+    checking: true,
+    // uid: null,
+    // name: null
+};
+
+export const authReducer = ( state = initalState, action ) => {
+
+    switch ( action.type ) {
+    
+        default:
+            return state;
+    }
+
+};
