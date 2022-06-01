ffb82732771546b75d1200fd853f5fe45baa67e3
Alex Paredes <alexparedesr95@gmail.com>
2022-05-31 20:25:15 -0500

    FEAT: Used useForm hook and dispatched the login form.

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import '../../styles/login.css';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { startLogin } from '../../actions/auth';
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+    
+    const [ formLoginValues, handleLoginInputChange ] = useForm({
+        loginEmail: 'alex@test.com',
+        loginPassword: '123456'
+    });
+
+    const { loginEmail, loginPassword } = formLoginValues;
+
+    const handleLogin = ( e ) => {
+        e.preventDefault();
+
+        dispatch( startLogin( loginEmail, loginPassword ) );
+    };
+
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={ handleLogin }>
                         <div className="form-group">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='loginEmail'
+                                value={ loginEmail }
+                                onChange={ handleLoginInputChange }
                             />
                         </div>
                         <div className="form-group">
@@ -20,6 +43,9 @@ export const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name='loginPassword'
+                                value={ loginPassword }
+                                onChange={ handleLoginInputChange }
                             />
                         </div>
                         <div className="form-group">
