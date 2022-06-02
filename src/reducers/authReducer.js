9d3edf85d5fd160432d8a54cc4716c1f6e5015ed
Alex Paredes <alexparedesr95@gmail.com>
2022-06-01 23:13:45 -0500

    FEAT: Handled case for authLogin type on authReducer.

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { types } from "../types/types";
+
 const initalState = {
     checking: true,
     // uid: null,
@@ -7,6 +9,13 @@ const initalState = {
 export const authReducer = ( state = initalState, action ) => {
 
     switch ( action.type ) {
+
+        case types.authLogin:
+            return {
+                ...state,
+                checking: false,
+                ...action.payload
+            };
     
         default:
             return state;
