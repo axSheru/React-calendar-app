ff08265bd7c19efca8b940298834ec6381d86139
Alex Paredes <alexparedesr95@gmail.com>
2022-06-04 22:39:05 -0500

    FEAT: Created action startChecking to renew token.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { fetchSinToken } from "../helpers/fetch";
+import { fetchConToken, fetchSinToken } from "../helpers/fetch";
 import { types } from "../types/types";
 import Swal from 'sweetalert2';
 
@@ -48,3 +48,27 @@ export const startRegister = ( email, password, name ) => {
 
     };
 };
+
+export const startChecking = () => {
+    return async ( dispatch ) => {
+
+        const resp = await fetchConToken( 'auth/renew' );
+        const body = await resp.json();
+
+        if ( body.ok ) {
+            localStorage.setItem( 'token', body.token );
+            localStorage.setItem( 'token-init-date', new Date().getTime() );
+
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+            }) );
+        } else {
+            Swal.fire( 'Error', body.msg, 'error' );
+            dispatch( checkingFinish() );
+        }
+
+    };
+};
+
+const checkingFinish = () => ({ type: types.authCheckingFinish });
