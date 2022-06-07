bc6da4deef4f70a66c77be864c3bbb0a2096b2ce
Alex Paredes <alexparedesr95@gmail.com>
2022-06-06 21:33:52 -0500

    FEAT: Checked if user is authenticating or not, added routes protections.

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,24 +1,43 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
     const dispatch = useDispatch();
+    const { checking, uid } = useSelector( state => state.auth );
 
     useEffect(() => {
         dispatch( startChecking() );
-    }, [ dispatch ])
+    }, [ dispatch ]);
+
+    if ( checking ) {
+        return (<h5>Loading...</h5>);
+    }
     
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/login' element={ <LoginScreen /> } />
-                <Route path='/' element={ <CalendarScreen /> } />
+                <Route path='/login' element={
+                    <PublicRoute
+                        isLoggedIn={ !! uid }
+                    >
+                        <LoginScreen />
+                    </PublicRoute>
+                } />
+                <Route path='/*' element={
+                    <PrivateRoute
+                        isLoggedIn={ !! uid }
+                    >
+                        <CalendarScreen />
+                    </PrivateRoute>
+                } />
                 <Route path='*' element={ <Navigate to='/' replace /> } />
             </Routes>
         </BrowserRouter>
