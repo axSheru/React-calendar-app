2cbec87bed2668a6d48518101793d326eda8b7b8
Alex Paredes <alexparedesr95@gmail.com>
2022-06-07 23:00:14 -0500

    FEAT: Created action startAddNewEvent to reach BE.

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,6 +1,37 @@
+import Swal from "sweetalert2";
+import { fetchConToken } from "../helpers/fetch";
 import { types } from "../types/types";
 
-export const eventAddNew = ( event ) => ({
+export const startEventAddNew = ( event ) => {
+    return async ( dispatch, getState ) => {
+
+        const { uid, name } = getState().auth;
+
+        try {
+            const resp = await fetchConToken( 'events', event, 'POST' );
+            const body = await resp.json();
+
+            if ( body.ok ) {
+
+                event.id = body.evento.id;
+                event.user = {
+                    _id: uid,
+                    name
+                };
+                
+                dispatch( eventAddNew( event ) );
+
+            }
+
+        } catch (error) {
+            Swal.fire( 'Error', error, 'error' );
+            console.log( error );
+        }
+
+    };
+};
+
+const eventAddNew = ( event ) => ({
     type: types.eventAddNew,
     payload: event
 });
