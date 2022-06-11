9fe939a8da868794c9fe65117a678026db922677
Alex Paredes <alexparedesr95@gmail.com>
2022-06-10 22:14:30 -0500

    FEAT: Created helper to change date format.

---
+++ src/helpers/prepareEvents.js
@@ -0,0 +1,13 @@
+import moment from "moment";
+
+export const prepareEvents = ( events = [] ) => {
+
+    return events.map(
+        ( e ) => ({
+            ...e,
+            start: moment( e.start ).toDate(),
+            end: moment( e.end ).toDate()
+        })
+    );
+
+};
